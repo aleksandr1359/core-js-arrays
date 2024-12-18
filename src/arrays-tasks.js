2c96e95024d0f0343f8917263511fe18f3f2bb01
aleksandr1359 <devyatiyarov.alexandr@gmail.com>
2024-12-18 22:54:06 +0500

    feat: passed 4 tests

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,8 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from({ length: end - start + 1 }, (v, k) => start + k);
 }
 
 /**
@@ -37,8 +37,10 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  return arr1 >= arr2
+    ? arr1.map((entry, i) => entry + (i < arr2.length ? arr2[i] : 0))
+    : arr2.map((entry, j) => entry + (j < arr1.length ? arr1[j] : 0));
 }
 
 /**
